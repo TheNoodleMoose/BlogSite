86229b4859427e3c2af971d97c6746d62cb1167f
Christian Huffman <cjh605@gmail.com>
2019-02-06 11:08:10 -0500

    Testing Routing of new pages

---
+++ PracticeSite/src/pages/company/team.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../../components/layout";
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor amet yr man braid prism, microdosing freegan cred
+        keffiyeh mixtape poutine vinyl. Pinterest fixie stumptown brunch DIY.
+        Everyday carry occupy austin, vegan narwhal flannel venmo tumeric lomo
+        gentrify af viral. Tote bag meh asymmetrical offal, chartreuse pitchfork
+        keytar plaid. Hashtag lyft vaporware ethical austin organic man braid
+        ennui meditation quinoa. Microdosing man braid listicle succulents.
+        Intelligentsia etsy adaptogen activated charcoal seitan.
+      </p>
+    </div>
+    <Link to="/">Go Home</Link>
+  </Layout>
+);
+
+export default Team;
--- PracticeSite/src/pages/index.js
+++ PracticeSite/src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
@@ -15,7 +15,11 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <br />
+    <Link to="/about/">Go to About Page</Link>
+    <br />
+    <Link to="/company/team">Go to Team Page</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
